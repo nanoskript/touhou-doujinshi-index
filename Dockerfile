1246e95b979f61c9eb861514cdfe24a671feab6d
Nanoskript <96655713+nanoskript@users.noreply.github.com>
2023-11-19 04:23:16 +1100

    feat: add Cython implementation of BK-tree

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM python:3.10-slim-buster
+FROM python:3.10-slim-bookworm
+
+RUN apt-get update && apt-get install -y g++
 
 RUN pip install --no-cache-dir pdm
 ADD ./pyproject.toml ./pdm.lock ./
--- scripts/entry_list_image_tree.py
+++ scripts/entry_list_image_tree.py
@@ -1,37 +1,35 @@
-import dataclasses
+import pyximport
 
-import pybktree
+pyximport.install()
 
+from .bktree import BKTree
 from .build_image_hashes import entry_h8s
 from .entry import Entry, EntryList
 from .utility import deduplicate_by_identity
 
 
-@dataclasses.dataclass()
-class BookGroupImageNode:
-    h8: int
-    group: EntryList | None
-
-
-def node_distance(a: BookGroupImageNode, b: BookGroupImageNode) -> int:
-    return (a.h8 ^ b.h8).bit_count()
-
-
 class EntryListImageTree:
-    tree: pybktree.BKTree
+    tree: BKTree
+    groups: dict[int, EntryList]
     orphans: list[Entry]
+
+    def _try_add_entry(self, h8s: list[int], group: EntryList):
+        for h8 in h8s:
+            if h8 not in self.groups:
+                self.groups[h8] = group
+                self.tree.add(h8)
 
     def __init__(self, initial_groups=None):
         if initial_groups is None:
             initial_groups = []
 
-        self.tree = pybktree.BKTree(node_distance)
+        self.tree = BKTree()
+        self.groups = {}
         self.orphans = []
 
         for group in initial_groups:
             for entry in group.entries:
-                for h8 in entry_h8s(entry):
-                    self.tree.add(BookGroupImageNode(h8=h8, group=group))
+                self._try_add_entry(entry_h8s(entry), group)
 
     def add_or_create(self, entry: Entry, similarity: float):
         h8s = entry_h8s(entry)
@@ -41,25 +39,18 @@ class EntryListImageTree:
 
         threshold = int((1.0 - similarity) * (8 * 8))
         for h8 in h8s:
-            query_node = BookGroupImageNode(h8=h8, group=None)
-            results = self.tree.find(query_node, threshold)
-
-            if results:
-                (_distance, node) = results[0]
-                node.group.entries.append(entry)
-                group = node.group
+            closest = self.tree.find_closest(h8, threshold)
+            if closest is not None:
+                group = self.groups[closest]
+                group.entries.append(entry)
                 break
         else:
             # No match found so create new group.
             group = EntryList(entries=[entry])
-
-        for h8 in h8s:
-            self.tree.add(BookGroupImageNode(h8=h8, group=group))
+        self._try_add_entry(h8s, group)
 
     def all_entry_lists(self) -> list[EntryList]:
-        groups = []
+        groups = list(self.groups.values())
         for orphan in self.orphans:
             groups.append(EntryList(entries=[orphan]))
-        for node in self.tree:
-            groups.append(node.group)
         return deduplicate_by_identity(groups)
